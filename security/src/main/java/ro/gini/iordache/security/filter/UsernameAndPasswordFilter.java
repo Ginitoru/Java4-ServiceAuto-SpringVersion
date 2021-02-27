92d7b1a14904d16cef369af6d9986a0205f91636
iordache <faraonu_ginitoru@yahoo.com>
2021-02-27 21:32:21 +0200

    finished 90% from UsernameAndPasswordFilter class

--- security/src/main/java/ro/gini/iordache/security/filter/UsernameAndPasswordFilter.java
+++ security/src/main/java/ro/gini/iordache/security/filter/UsernameAndPasswordFilter.java
@@ -1,7 +1,10 @@
 package ro.gini.iordache.security.filter;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import ro.gini.iordache.security.authentication.UserNamePasswordAuthentication;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -19,9 +22,22 @@ public class UsernameAndPasswordFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest,
-                                    HttpServletResponse httpServletResponse,
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
+
+
+        var usernameOrEmail = request.getParameter("username");
+        var password = request.getParameter("password");
+
+
+        Authentication auth = new UserNamePasswordAuthentication(usernameOrEmail, password);
+
+        auth = authenticationManager.authenticate(auth);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        filterChain.doFilter(request, response);
     }
+
+    
 }
