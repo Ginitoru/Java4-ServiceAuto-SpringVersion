8568f488d398633a34ebc433dc7e001320f949f3
iordache <faraonu_ginitoru@yahoo.com>
2021-02-27 19:40:49 +0200

    added domain dependency to persitance module and made UserDao interface

---
+++ persistence/src/main/java/com/gini/iordache/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.gini.iordache.dao;
+
+
+import com.gini.iordache.entity.User;
+
+import java.util.Optional;
+
+public interface UserDao {
+
+    void createUser(User user);
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+    
+}
