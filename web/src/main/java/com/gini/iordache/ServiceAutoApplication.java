b933c443c88fc1582a2d0dc0eaeb458dbb9a7528
iordache <faraonu_ginitoru@yahoo.com>
2021-02-27 17:54:06 +0200

    configre the project: adding dependencies, creating 3 modules: domain, persistance and web. Establishing connection to database, adding .gitignore file. Starting Spring Boot project: Java4-ServiceAuto-SpringVersion -> final project for Java 4course @telacad.ro.

---
+++ web/src/main/java/com/gini/iordache/ServiceAutoApplication.java
@@ -0,0 +1,14 @@
+package com.gini.iordache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAutoApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServiceAutoApplication.class, args);
+
+    }
+
+}
