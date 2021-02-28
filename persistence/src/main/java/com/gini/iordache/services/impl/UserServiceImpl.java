80e8b66235d8997c890e1d8c383fcb579638fa70
iordache <faraonu_ginitoru@yahoo.com>
2021-02-28 21:05:51 +0200

    added BCryptPasswordEncoder in SecurityConfiguration. Made modification in CreateUser method form UserServiceImpl so now when a user is created the password is encoded with BCryptPasswordEncoder

--- persistence/src/main/java/com/gini/iordache/services/impl/UserServiceImpl.java
+++ persistence/src/main/java/com/gini/iordache/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.gini.iordache.dao.UserDao;
 import com.gini.iordache.entity.User;
 import com.gini.iordache.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,12 +16,13 @@ public class UserServiceImpl implements UserService {
 
 
     private final UserDao userDao;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserDao userDao) {
+    public UserServiceImpl(UserDao userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
-
 
     @Override
     @Transactional
@@ -30,6 +32,10 @@ public class UserServiceImpl implements UserService {
         Optional<User> user2 = userDao.findUserByEmail(user.getEmail());
 
         if(user1.isEmpty() && user2.isEmpty()){
+
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+
             userDao.createUser(user);
 
         }else{
