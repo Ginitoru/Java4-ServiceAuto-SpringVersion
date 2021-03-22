cff626fd35fa706bb126a54db8baee5058f146d1
iordache <faraonu_ginitoru@yahoo.com>
2021-03-22 20:38:34 +0200

    created LaborConverto.class -> converts Labor instances to LaborServiceOrder instances to be persisted to the database

--- domain/src/main/java/com/gini/iordache/entity/order/LaborServiceOrder.java
+++ domain/src/main/java/com/gini/iordache/entity/order/LaborServiceOrder.java
@@ -2,12 +2,14 @@ package com.gini.iordache.entity.order;
 
 
 import com.gini.iordache.utility.LaborCategory;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -32,4 +34,36 @@ public class LaborServiceOrder {
     @Enumerated(EnumType.STRING)
     private LaborCategory category;
 
+    public LaborServiceOrder(String laborDescription, double timedLabor, double laborPrice, ServiceOrder serviceOrder, LaborCategory category) {
+        this.laborDescription = laborDescription;
+        this.timedLabor = timedLabor;
+        this.laborPrice = laborPrice;
+        this.serviceOrder = serviceOrder;
+        this.category = category;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LaborServiceOrder that = (LaborServiceOrder) o;
+        return Double.compare(that.timedLabor, timedLabor) == 0 && Double.compare(that.laborPrice, laborPrice) == 0 && Objects.equals(laborDescription, that.laborDescription) && category == that.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(laborDescription, timedLabor, laborPrice, category);
+    }
+
+    @Override
+    public String toString() {
+        return "LaborServiceOrder{" +
+                "id=" + id +
+                ", laborDescription='" + laborDescription + '\'' +
+                ", timedLabor=" + timedLabor +
+                ", laborPrice=" + laborPrice +
+                ", category=" + category +
+                '}';
+    }
 }
---
+++ persistence/src/main/java/com/gini/iordache/convertor/LaborConvertor.java
@@ -0,0 +1,28 @@
+package com.gini.iordache.convertor;
+
+import com.gini.iordache.entity.labor.Labor;
+import com.gini.iordache.entity.order.LaborServiceOrder;
+import com.gini.iordache.entity.order.ServiceOrder;
+
+public class LaborConvertor {
+
+    private LaborConvertor() {
+    }
+    
+
+    public static LaborServiceOrder convert(Labor labor, double laborPrice, ServiceOrder serviceOrder){
+
+        LaborServiceOrder laborServiceOrder = new LaborServiceOrder(
+                                                                labor.getLaborDescription(),
+                                                                labor.getTimedLabor(),
+                                                                laborPrice,
+                                                                serviceOrder,
+                                                                labor.getCategory()
+
+        );
+
+
+        return laborServiceOrder;
+
+    }
+}
