22febdd4dbf106dc90a45b7615c2f36440a0969b
iordache <faraonu_ginitoru@yahoo.com>
2021-03-29 21:41:28 +0300

    created aop module, added dependecies for Spring AOP and AspectJ

--- security/src/main/java/ro/gini/iordache/security/configuration/SecurityConfiguration.java
+++ security/src/main/java/ro/gini/iordache/security/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -27,6 +28,7 @@ import ro.gini.iordache.security.provider.UserNamePasswordProvider;
 
 @Configuration
 @EnableAsync
+@EnableAspectJAutoProxy
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
