0f1309605cbd1f185a2cb872387c4b5d09ef6793
iordache <faraonu_ginitoru@yahoo.com>
2021-04-19 21:33:19 +0300

    renamed account.html to account-info.html and added acont-info.css to it

--- web/src/main/java/com/gini/iordache/controllers/user/ActivateAccountController.java
+++ web/src/main/java/com/gini/iordache/controllers/user/ActivateAccountController.java
@@ -3,7 +3,6 @@ package com.gini.iordache.controllers.user;
 import com.gini.iordache.entity.user.ActivationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +28,7 @@ public class ActivateAccountController {
     @GetMapping("/account")
     public String accountActivePage(){
 
-        return "exceptions/account";
+        return "exceptions/account-info";
 
     }
 }
